aa5a1b4ddab86c8e816950b2e64dbe7be1d1c829
Luis <lromeror@fiec.espol.edu.ec>
2023-08-11 21:38:26 -0500

    Cambios en la page

--- APP/index.py
+++ APP/index.py
@@ -4,13 +4,14 @@ import dash_bootstrap_components as dbc
 import os
 import plotly.express as px
 import pandas as pd
-# bootstrap theme
-# https://bootswatch.com/lux/
+import math
 external_stylesheets = [dbc.themes.BOOTSTRAP]  
-# external_stylesheets = [dbc.themes.SANDSTONE]
 from dash import Dash, dcc, html,callback
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
+# bootstrap theme
+# https://bootswatch.com/lux/
+# external_stylesheets = [dbc.themes.SANDSTONE]
 
 
 app = Dash(__name__, suppress_callback_exceptions=True,external_stylesheets=external_stylesheets) #external_stylesheets=external_stylesheets
@@ -70,59 +71,119 @@ try:
 except FileNotFoundError:
     df = pd.read_csv("https://raw.githubusercontent.com/lromeror/ProyectoCV/main/APP/assets/data/planificacion.csv",sep=",")
     #ahí que estar actualizando
-finally:
-    print(df)
-bosy=html.Div([
+
+
+distancia=0
+menuHorario=html.Div([
     dbc.Container([
     dbc.Row([
         # Sidebar
         dbc.Col([
-            html.H2('Ubicaciones'),
-            html.H6('Origen:'),
+            html.H3('Ubicaciones de Partida'),
+            html.H6('Materia:'),
             dcc.Dropdown(
                 
-                id='dropdown1',
-                options=[
-                    {'label': 'Opción 1', 'value': 'opt1'},
-                    {'label': 'Opción 2', 'value': 'opt2'},
-                    {'label': 'Opción 3', 'value': 'opt3'}
-                ],style={'margin-bottom': '50px'},
-                value='opt1'
-            ),html.H6('Llegada:'),
+                id='Materiapartida',
+                options=df.NOMBRE,style={'margin-bottom': '30px'},
+                value=df.NOMBRE.unique().tolist()[0]
+            ),html.H6('Bloque:'),
             dcc.Dropdown(
                 
-                id='dropdown2',
-                options=[
-                    {'label': 'A', 'value': 'A'},
-                    {'label': 'B', 'value': 'B'},
-                    {'label': 'C', 'value': 'C'}
-                ],
-                value='A'
+                id='Bloquepartida',
+                options=df.BLOQUE,style={'margin-bottom': '50px'},
+                value=df.BLOQUE.unique().tolist()[0]
             ),
-        ], width=2, style={'padding': '20px', 'background-color': '#889694 ',"border-radius": "5px"}),
-
+        ], width=5, style={'padding': '20px', 'background-color': '#889694 ',"border-radius": "5px"}),
+        dbc.Col([html.H1(" ")],width=2),
         # Contenido principal
         dbc.Col([
-            html.H1('Aqui va el mapa'),
-        ], width=9, style={'padding': '20px'}),
+            html.H3('Ubicaciones de Llegada'),
+            html.H6('Materia:'),
+            dcc.Dropdown(
+                
+                id='MateriaLlegada',
+                options=df.NOMBRE,style={'margin-bottom': '30px'},
+                value=df.NOMBRE.unique().tolist()[1]
+            ),html.H6('Bloque:'),
+            dcc.Dropdown(
+                id='BloqueLlegada',
+                options=df.BLOQUE,style={'margin-bottom': '50px'},
+                value=df.BLOQUE.unique().tolist()[1]
+            ),
+        ], width=5, style={'padding': '20px', 'background-color': '#889694 ',"border-radius": "5px"})
     ]),
-],fluid=True,className=" mt-4 g-0",style={"background-color": "#f2f2f2", "padding": "50px","border-radius": "10px"})#container
+],fluid=True,className=" container mt-4 g-0",style={"background-color": "#f2f2f2", "padding": "50px","border-radius": "10px"})#container
 ])
 
-#map = dbc.Container([
-#    html.Iframe(src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.0205291828083!2d-79.96704509721975!3d-2.1458240017673655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x902d72f925e5bfdb%3A0x327cdb9f7f4ba3b2!2sEscuela%20Superior%20Polit%C3%A9cnica%20del%20Litoral%20(ESPOL)!5e0!3m2!1ses!2sec!4v1691779963691!5m2!1ses!2sec" ,width="600" ,height="450")
-#])
+@app.callback([Output('')],[Input('Materiapartida', 'value'), Input('Bloquepartida', 'value'), Input('MateriaLlegada', 'value'), Input('BloqueLlegada', 'value')])
+def grafica(Materiapartida,Bloquepartida,MateriaLlegada,BloqueLlegada):
+    global distancia
+    lat1=df[(df['NOMBRE']==Materiapartida) & (df['BLOQUE']==Bloquepartida)]['lat'].values[0]
+    lon1=df[(df['NOMBRE']==Materiapartida) & (df['BLOQUE']==Bloquepartida)]['lot'].values[0]
+
+    lat2=df[(df['NOMBRE']==MateriaLlegada) & (df['BLOQUE']==BloqueLlegada)]['lat'].values[0]
+    lon2=df[(df['NOMBRE']==MateriaLlegada) & (df['BLOQUE']==BloqueLlegada)]['lot'].values[0]
+    distancia= haversine_distance(lat1,lon1,lat2,lon2)
+    
+
+#Funcion para calcular la distancia entre dos puntos
+def haversine_distance(lat1, lon1, lat2, lon2):
+    # Radio de la Tierra en kilómetros
+    R = 6371.0
+
+    # Convertir grados a radianes
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+
+    # Diferencia de coordenadas
+    dlat = lat2_rad - lat1_rad
+    dlon = lon2_rad - lon1_rad
+
+    # Fórmula del haversine
+    a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    # Distancia en kilómetros
+    distance = R * c
+
+    # Convertir a metros
+    distance_meters = distance * 1000
+
+    return distance_meters
 
 
-# embedding the navigation bar
-tituloProyecto=dbc.Container([
-    dbc.Row([
-        dbc.Col(html.H3("OPTIMIZACION DE RUTAS EN LAS AULAS CLASES"),className="col1_1"),
-    ])
+us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+us_cities = us_cities.query("State in ['New York', 'Ohio']")
+grafica=html.Div([
+    dcc.Graph(id="city-map"),
+])
 
+
+@app.callback(
+    Output("city-map", "figure"),
+    Input("city-map", "relayoutData")
+)
+def update_map(relayoutData):
+    # Crear la figura del mapa
+    fig = px.line_mapbox(us_cities, lat="lat", lon="lon", color="State", zoom=3, height=800)
+    
+    fig.update_layout(mapbox_style="stamen-terrain", mapbox_zoom=4, mapbox_center_lat=41,
+                    margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    
+    return fig
+
+mapa=html.Div([
+    dbc.Container([
+        dbc.Row([
+            dbc.Col(html.H3("Mapa de Rutas",className="text-center")),
+        ]),
+    dbc.Row(grafica),
+],fluid=True,className=" container mt-4 g-0",style={"background-color": "#f2f2f2", "padding": "50px","border-radius": "10px"})#container
 ])
 app.layout = html.Div([
-    navbar2,tituloProyecto,bosy
+    navbar2,tituloProyecto,menuHorario,mapa
 ])
 
 
